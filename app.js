483464977cfd318070dea04a3d31f9f72aa72a77
img4 <img4@dw1.xyz>
2025-05-20 21:19:15 -0700

    Update page

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ let userRepo
 if (location.pathname.indexOf("C:") !== -1) userRepo = 'img4/i' // local dev
 else userRepo = location.host.split('.')[0] + '/' + location.pathname.split('/')[1]
 let id = (location.search ? location.search.substring(1) : '').split('&')[0]
-let cacheBusting
 
 $(() => {
-	// remove cache buster from address bar
-	if(location.search.indexOf('&')!==-1){
-		cacheBusting=true
-		history.replaceState(null, '', location.href.split('&')[0]);
-	}
 	// on load, view one image or gallery
 	if (id) { // view one image, with link to gallery
 		// bind left and right keys to increase or decrease id
@@ -35,8 +29,7 @@ $(() => {
 				$('title').text(r.p)
 				$('body').append('<div class="container"><h1 class="header">' + r.p + '</h1><div class="image-wrapper"><img src="' + r.i + '" alt=""><div class="footer">' + r.m + '</div></div></div>')
 			} else {
-				// $('body').html('<div id="notfound"><b>Image not found</b><br>New images can take a few seconds<br><a class="btn btn-primary" href="javascript:location.href=location.href.split(\'&\')[0]+\'&\'+Date.now()">Refresh</a><br>Ctrl-F5 may work better</div>')
-				$('body').html('<div id="notfound"><b>Image '+id+' not found</b><br>New images can take a few seconds<br><a class="btn btn-primary" href="javascript:location.reload()">Refresh</a><br><div id="auto-refresh">Auto-refresh in 10</div><br>⬅ Navigate with arrow keys ➡<!--<br>Ctrl-F5 may work better--></div>')
+				$('body').html('<div id="notfound"><b>Image '+id+' not found</b><br>New images can take a few seconds to propagate<br><a class="btn btn-primary" href="javascript:location.reload()">Refresh</a><br><div id="auto-refresh">Auto-refresh in 10</div><br>⬅ Navigate with arrow keys ➡<!--<br>Ctrl-F5 may work better--></div>')
 				var start=Date.now(), next=Date.now() + 10000, iv=setInterval(()=> {
 					// if(Date.now()-start > 300000){ $('#auto-refresh').html('Auto-refresh stopped after 5m'); clearInterval(iv) }
 					let when = (Date.now() - next) * -1;
@@ -57,22 +50,17 @@ $(() => {
 })
 
 function loadImageByID(id, cb) {
-	let url = 'https://raw.githubusercontent.com/' + userRepo + '/HEAD/images/' + id[0] + '/' + (id.length > 1 ? id[1] : '0') + '/' + id
-	// if(cacheBusting) url += '?' + Date.now()
-	url += '?' + Date.now()
+	let url = 'https://raw.githubusercontent.com/' + userRepo + '/HEAD/images/' + id[0] + '/' + (id.length > 1 ? id[1] : '0') + '/' + id + '?' + Date.now()
 	console.log('source url: ', url)
 	$.get(url)
 		.done(r => {
 			try {
 				data = JSON.parse(r)
-				/*
-										{ // service, model, prompt, type, image data
-											"s": "Gemini",
-											"m": "imagen-3-generate-002",
-											"p": "funny picture of a shrew", (b64)
-											"t": "image/png",
-											"i": "..." (data uri)
-										}*/
+				/*{ // service, model, prompt, type, image data
+					"m": "imagen-3-generate-002",
+					"p": "funny picture of a shrew", (b64)
+					"i": "..." (data uri)
+				}*/
 				data.p = b64Decode(data.p)
 				console.log('image data:', data)
 				cb(data)
